bf4596bfb80ce28e888e548d709d0deb3c0d1ae9
Андрей Вицентович < >
2016-09-04 17:08:40 +0300

    Second commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 
 var calcForm = document.calc;
 var expr = '';
+var operArr = ['+', '-', '*', '/', '%'];
+var expArr = expr.split(',');
 
 function NumPressed(symb) {
         
@@ -12,15 +14,55 @@ function NumPressed(symb) {
 
 
 function Neg() {
+
+    for (var i = expArr.length-1; i > 0; i--){
+
+        if(expArr[i] === '-'){
+            
+            expArr[i] = '+';
+            exp = expArr.join('');
+            return;
+        }
     
-    var expArr = expr.split('');
-    if (expArr[expArr.length-1] === '-') {
+        if(expArr[i] === '+'){
+            
+                expArr[i] = '-';
+                exp = expArr.join('');
+                return;
+            }
+        if(expArr[i] === '*' || '/' || '%'){
+                
+                shiftRight(i);
+                expArr[i] = '-';
+                exp = expArr.join();
+                return;
+            }
+        
+    }
+    
+}
+
+
+function shiftRight(pos){
+    
+    expArr.push(' ');
+    for (var i = expArr.length-1; i > pos; i--){
+        expArr[i] = expArr[i-1];
+    }
+}
+
+
+function insRemoveMin(pos){
+    
+    if (expArr[pos-1] === '-') {
         
         expArr[expArr.length-1] = expArr[expArr.length];
         expArr.pop();
+        expr = expArr.join();
         console.log(expr);
         return;
     }
+    
     var buf = expArr[expArr.length-1];
     expArr[expArr.length-1] = '-';
     expArr.push(buf);
@@ -28,6 +70,7 @@ function Neg() {
     console.log(expr);
     
 }
+
 //function inputshow (NumPressed){
 //    
 //    var elem = calcForm.elements['ReadOut'];
