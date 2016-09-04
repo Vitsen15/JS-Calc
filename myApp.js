ce543756a4ca95b7cf2d66c5d406efb2ba45d6c1
Андрей Вицентович < >
2016-09-04 18:30:55 +0300

    Operations

--- myApp.js
+++ myApp.js
@@ -2,39 +2,41 @@
 var calcForm = document.calc;
 var expr = '';
 var operArr = ['+', '-', '*', '/', '%'];
-var expArr = expr.split(',');
+//var expArr = expr.split(',');
 
-function NumPressed(symb) {
+function NumPressed (symb) {
         
     calcForm.ReadOut.value = symb;
-    expr = expr + symb;
+    expr = expr + symb + '';
     console.log(expr);    
              
 }
 
 
-function Neg() {
+function Neg () {
+    var expArr = expr.split(' ');
+    
 
     for (var i = expArr.length-1; i > 0; i--){
 
         if(expArr[i] === '-'){
             
             expArr[i] = '+';
-            exp = expArr.join('');
+            expr = expArr.join('');
             return;
         }
     
         if(expArr[i] === '+'){
             
                 expArr[i] = '-';
-                exp = expArr.join('');
+                expr = expArr.join('');
                 return;
             }
         if(expArr[i] === '*' || '/' || '%'){
                 
                 shiftRight(i);
                 expArr[i] = '-';
-                exp = expArr.join();
+                expr = expArr.join();
                 return;
             }
         
@@ -43,33 +45,48 @@ function Neg() {
 }
 
 
-function shiftRight(pos){
+function shiftRight (pos){
     
+    var expArr = expr.split(' ');
     expArr.push(' ');
+    
     for (var i = expArr.length-1; i > pos; i--){
         expArr[i] = expArr[i-1];
     }
 }
 
-
-function insRemoveMin(pos){
+function Operation (operation){
     
-    if (expArr[pos-1] === '-') {
-        
-        expArr[expArr.length-1] = expArr[expArr.length];
-        expArr.pop();
-        expr = expArr.join();
-        console.log(expr);
+    var expArr = expr.split(' ');
+    if (expArr[expArr.length-1] === '*' || '/' || '%' || '+' || '-'){
         return;
     }
-    
-    var buf = expArr[expArr.length-1];
-    expArr[expArr.length-1] = '-';
-    expArr.push(buf);
-    expr = expArr.join('');
-    console.log(expr);
+    expArr.push(operation);
+    expr = expArr.join();
     
 }
+
+
+
+
+//function insRemoveMin(pos){
+//    
+//    if (expArr[pos-1] === '-') {
+//        
+//        expArr[expArr.length-1] = expArr[expArr.length];
+//        expArr.pop();
+//        expr = expArr.join();
+//        console.log(expr);
+//        return;
+//    }
+//    
+//    var buf = expArr[expArr.length-1];
+//    expArr[expArr.length-1] = '-';
+//    expArr.push(buf);
+//    expr = expArr.join('');
+//    console.log(expr);
+//    
+//}
 
 //function inputshow (NumPressed){
 //    
