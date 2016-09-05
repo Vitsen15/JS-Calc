1df5f6fb162f4c2d57590f12ce6425f830d2d064
Андрей Вицентович < >
2016-09-05 16:22:17 +0300

    Operations

--- myApp.js
+++ myApp.js
@@ -1,68 +1,90 @@
 
 var calcForm = document.calc;
 var expr = '';
-var operArr = ['+', '-', '*', '/', '%'];
-//var expArr = expr.split(',');
+//var operArr = ['+', '-', '*', '/', '%'];
 
 function NumPressed (symb) {
         
     calcForm.ReadOut.value = symb;
-    expr = expr + symb + '';
+    expr = expr + symb;
     console.log(expr);    
              
 }
 
 
 function Neg () {
-    var expArr = expr.split(' ');
     
-
+    var expArr = expr.split('');
+    
     for (var i = expArr.length-1; i > 0; i--){
-
-        if(expArr[i] === '-'){
+        
+        var buf = expArr[i];
+        
+        if(buf === '-'){
             
             expArr[i] = '+';
             expr = expArr.join('');
+            console.log(expr);
             return;
         }
     
-        if(expArr[i] === '+'){
+        if(buf === '+'){
             
                 expArr[i] = '-';
                 expr = expArr.join('');
+                console.log(expr);
                 return;
             }
-        if(expArr[i] === '*' || '/' || '%'){
+        if( buf === '*' ||
+            buf === '/' ||
+            buf === '%' ||  
+            buf === '*') {
                 
-                shiftRight(i);
-                expArr[i] = '-';
-                expr = expArr.join();
+                shiftRight(i, '-');
+                console.log(expr);
                 return;
             }
+        if (i == 0){
+            shiftRight(i, '-');
+            console.log(expr);
+            return;
+        }
         
     }
     
 }
 
 
-function shiftRight (pos){
+function shiftRight (pos , symb){
     
-    var expArr = expr.split(' ');
+    var expArr = expr.split('');
     expArr.push(' ');
     
     for (var i = expArr.length-1; i > pos; i--){
         expArr[i] = expArr[i-1];
     }
+    expArr[pos+1] = symb;
+    expr = expArr.join('');
 }
 
 function Operation (operation){
     
-    var expArr = expr.split(' ');
-    if (expArr[expArr.length-1] === '*' || '/' || '%' || '+' || '-'){
+    var expArr = expr.split('');
+    var buf = expArr[expArr.length-1];
+    
+    if (buf === '*' ||
+        buf === '/' ||
+        buf === '%' || 
+        buf === '+' ||
+        buf === '-' ) {
+        expArr[expArr.length-1] = operation;
+        expr = expArr.join('');
+        console.log(expr);
         return;
     }
     expArr.push(operation);
-    expr = expArr.join();
+    expr = expArr.join('');
+    console.log(expr);
     
 }
 
